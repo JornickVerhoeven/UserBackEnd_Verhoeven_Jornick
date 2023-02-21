d256948129b333b315059e81c6d9d118443028ca
Jornick Verhoeven <jornick.verhoeven@student.ucll.be>
2023-02-21 10:29:38 +0100

    les 1

--- 01_Labo_BackEndUsers_Model-main/src/main/java/demo/User.java
+++ 01_Labo_BackEndUsers_Model-main/src/main/java/demo/User.java
@@ -8,36 +8,57 @@ public class User {
     private String name;
     private int age;
     private List<Integer> membershipYears = new ArrayList<Integer>();
+    private String email;
+    private String password;
 
-    public User(String name, int age) {
+    public User(String name, int age, String email, String password) {
         this.name = name;
-        if (age >= 0) 
+        this.email = email;
+        this.password = "@$-" + password + "&%#";
+
+        if (age >= 0)
             this.age = age;
     }
 
-    public int countMembershipYearsAfter1999 () {
+    public int countMembershipYearsAfter1999() {
         int result = 0;
-        for(Integer year: membershipYears) {
+        for (Integer year : membershipYears) {
             if (year > 1999)
                 result++;
         }
         return result;
     }
 
-    public int countYearsOfMembership () {
+    public int countYearsOfMembership() {
         return membershipYears.size();
     }
 
-    public void addMembershipYear (int year) {
+    public void addMembershipYear(int year) {
         membershipYears.add(year);
+    }
+
+    public int getFirstMembershipYear() {
+        int year = membershipYears.get(0);
+        for (int i = 0; i < membershipYears.size(); i++) {
+            if (membershipYears.get(i + 1) < membershipYears.get(i))
+                year = membershipYears.get(i);
+        }
+        return year;
     }
 
     public int getAge() {
         return this.age;
     }
 
-    public String getName () {
+    public String getName() {
         return name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
